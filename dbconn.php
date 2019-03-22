5d1e46ecc2e72e0794b461d17b56996bdc9addaa
thegr8shoaib <45881059+thegr8shoaib@users.noreply.github.com>
2019-03-22 19:02:11 +0500

    learning new things i have changed 3 files
    learning new things i have changed 3 files

---
+++ dbconn.php
@@ -0,0 +1,9 @@
+<?php
+
+
+ mysqli_connect('localhost','root','') or die('colud not connect to database');
+echo "connect";
+
+
+
+ ?>
---
+++ preg.php
@@ -0,0 +1,13 @@
+
+<?php
+
+$string ="hello world";
+if (preg_match('/hello /',$string)) {
+  echo "match  found";
+
+}else {
+  echo "match not found";
+}
+
+
+ ?>
